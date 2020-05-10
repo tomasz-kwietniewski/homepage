6be23474bd31db0ee71d341a9d911448e9afb5ee
Tomasz Kwietniewski <tomasz.kwietniewski@gmail.com>
2020-05-10 22:15:48 +0200

    added document.querySelector to JS and changed value of new element by innerHTML

--- main.js
+++ main.js
@@ -4,3 +4,9 @@ const age = 33;
 console.log(firstName);
 console.log(age);
 console.log(`Mam na imię ${firstName} i mam ${age} lata.`);
+
+const article = document.querySelector('.notes__article--js');
+console.log(article.innerHTML)
+
+article.innerHTML = '<p>LEGIA MISTRZ</p>'
+console.log(article.innerHTML)
