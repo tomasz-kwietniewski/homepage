f3c0d2a4aad60818c4ce5b74a0721b4e12ca1930
Tomasz Kwietniewski <tomasz.kwietniewski@gmail.com>
2020-05-11 21:08:01 +0200

    centered by class .main and corrected BEM

--- main.js
+++ main.js
@@ -5,8 +5,8 @@ console.log(firstName);
 console.log(age);
 console.log(`Mam na imię ${firstName} i mam ${age} lata.`);
 
-const article = document.querySelector('.notes__article--js');
-console.log(article.innerHTML)
+const article = document.querySelector(".notes__article--js");
+console.log(article.innerHTML);
 
-article.innerHTML = '<p>LEGIA MISTRZ</p>'
-console.log(article.innerHTML)
+article.innerHTML = "<p>LEGIA MISTRZ</p>";
+console.log(article.innerHTML);
